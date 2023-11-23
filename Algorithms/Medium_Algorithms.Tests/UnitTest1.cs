2bd69fa693f388345b4f07889dc0148db20d4ead
Khotso Charles Mokhethi <Mokhetkc@hotmail.com>
2023-11-23 21:08:52 +0200

    .

--- Algorithms/Medium_Algorithms.Tests/UnitTest1.cs
+++ Algorithms/Medium_Algorithms.Tests/UnitTest1.cs
@@ -743,10 +743,42 @@ namespace Medium_Algorithms.Tests
                 }
             }
             return true;
-        } 
+        }
         #endregion
 
+        #region RiverSizesClass
+        [Fact]
+        public void RiverSizesClassTestCase1()
+        {
+            int[,] input = {
+              { 1, 0, 0, 1, 0 },
+              { 1, 0, 1, 0, 0 },
+              { 0, 0, 1, 0, 1 },
+              { 1, 0, 1, 0, 1 },
+              { 1, 0, 1, 1, 0 },
+            };
+            int[] expected = { 1, 2, 2, 2, 5 };
+            List<int> output = RiverSizesClass.RiverSizes(input);
+            output.Sort();
+            Assert.True(compare(output, expected));
+        }
 
+        public static bool compare(List<int> arr1, int[] arr2)
+        {
+            if (arr1.Count != arr2.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < arr1.Count; i++)
+            {
+                if (arr1[i] != arr2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        } 
+        #endregion
 
 
 
